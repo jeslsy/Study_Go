2e21cd828a5186d417e383a352cd9255a87298de
jeslsy0507 <jeslsy0507@naver.com>
2021-03-18 20:42:28 +0900

    finish account

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -46,3 +46,7 @@ func (a *Account) ChangeOwner(newOwner string) {
 func (a Account) Owner() string {
 	return a.owner
 }
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
+}
--- main.go
+++ main.go
@@ -9,10 +9,5 @@ import (
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(1000)
-
-	err := account.Withdraw(3000)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance(), account.Owner())
+	fmt.Println(account)
 }
